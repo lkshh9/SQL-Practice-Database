4d9c2f818b56f6e6be007391130913c00f08f54a
Lokesh Kumawat <lokeshmarwal007@gmail.com>
2024-05-30 11:34:09 +0530

    Order By clause queries

---
+++ OrderBy.sql
@@ -0,0 +1,53 @@
+SELECT
+    first_name,
+    last_name
+FROM
+    sales.customers
+ORDER BY
+    first_name;
+
+SELECT
+	first_name,
+	last_name
+FROM
+	sales.customers
+ORDER BY
+	first_name DESC;
+
+SELECT
+    city,
+    first_name,
+    last_name
+FROM
+    sales.customers
+ORDER BY
+    city,
+    first_name;
+
+SELECT
+    city,
+    first_name,
+    last_name
+FROM
+    sales.customers
+ORDER BY
+    city DESC,
+    first_name ASC;
+
+SELECT
+    first_name,
+    last_name
+FROM
+    sales.customers
+ORDER BY
+    LEN(first_name) DESC;
+
+
+SELECT
+    city,
+    first_name,
+    last_name
+FROM
+    sales.customers
+ORDER BY
+    state;
